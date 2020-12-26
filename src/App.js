9d15b9efcc1c16fadd4bc1b07b9e8a5683c95e78
Zoë Davidson <zoe@Zoes-Macbook.local>
2020-12-25 22:41:17 -0400

    second page

--- src/App.js
+++ src/App.js
@@ -14,9 +14,19 @@ function App() {
         </div>
       </div>
       <div class="secondPage">
-        <div class="who">
-          
+        <div class="who clearfix">
+          <h3 class="smallTitle">Who I Am</h3>
+            <h4 class="bigTitle">Filmmaker + Front-End Web Developer</h4>
+              <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </div>
+          <div class="pageLinks">
+              <a class="sectionLink" href="#">Websites</a>
+              <a class="sectionLink" href="#">Films</a>
+              <a class="sectionLink" href="#">Photography</a>
+          </div>
+      </div>
+      <div class="thirdPage">
+
       </div>
     </div>
   );
