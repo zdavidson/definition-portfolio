1d8895d083b64bde88df1a2f6df2c8a4312cd967
Zoë Davidson <zoe@Zoes-Macbook.local>
2020-12-25 22:13:14 -0400

    first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import './resources/normalize.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Zoe Davidson</h1>
+      <div class="firstPage">
+        <div class="title">
+          <h2 class="phonetic">Zo·ë Da·vid·son</h2>
+            <h3 class="phonetic">/ˈzoʊ.i/ [dˈe͡ɪvɪdsən]</h3>
+              <h4 class="noun">noun</h4>
+                <p class="definition">a filmmaker and front-end developer from Toronto, Canada</p>
+        </div>
+      </div>
+      <div class="secondPage">
+        <div class="who">
+          
+        </div>
+      </div>
     </div>
   );
 }
